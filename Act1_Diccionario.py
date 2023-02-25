00d1a14ccb5571981c0ae819de06a34f65bb1131
IDGS-803-20001475 <20001475@alumnos.utleon.edu.mx>
2023-02-25 04:38:10 -0600

    clase 25/02/23

--- Act1_Diccionario.py
+++ Act1_Diccionario.py
@@ -8,9 +8,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY']="Esta es la clave encriptada"
 csrf=CSRFProtect()
 
-@app.errorhandler(404)
-def no_encontrada(e):
-    return render_template('404.html'),404
 @app.route("/cookies", methods=['GET','POST'])
 def cookies():
     reg_user=forms.LoginForm(request.form)
@@ -27,12 +24,6 @@ def cookies():
         response.set_cookie('datos_user',datos)
     return response
 
-@app.route("/saludo")
-def saludo():
-    valor_cookie=request.cookies.get('datos_user')
-    nombres=valor_cookie.split('@')
-    return render_template('saludo.html',nom=nombres[0])
-
 
 
 
@@ -42,13 +33,16 @@ def saludo():
 def formulario():
     return render_template('formulario.html')
 
-@app.route("/alumnos", methods=['GET','POST'])
-def alumnos():
-    alum_form=forms.UserForm(request.form)
-    if request.method=='POST' and alum_form.validate():
-        print(alum_form.matricula.data)
-        print(alum_form.nombre.data)
-    return render_template('Alumnos.html',form=alum_form)
+@app.route("/palabras", methods=['GET','POST'])
+def palabras():
+    trad_form=forms.traductor(request.form)
+    if request.method=='POST' and trad_form.validate():
+        palabra= str(trad_form.palabra.data)
+        word= str(trad_form.word.data)
+        f=open('diccionario.txt','a')
+        f.write('\n'+palabra.upper()+' = '+word.upper())
+        f.close()
+    return render_template('Act1_Diccionario.html',form=trad_form)
 
 
 
--- forms.py
+++ forms.py
@@ -3,10 +3,40 @@ from wtforms import Form
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FieldList, FormField, SelectField, RadioField
 from wtforms.fields import EmailField
+from wtforms import validators
 
-
+def mi_validacion(form,field):
+    if len(field.data)==0:
+        raise validators.ValidationError('El campo no tiene datos')
+    
 class UserForm(Form):
-    matricula=StringField('Matricula')
-    nombre=StringField('Nombre')
-    apaterno=StringField('Apaterno')
+    matricula=StringField('Matricula',[
+        validators.DataRequired(message="El campo es requerido"),
+        validators.length(min=4,max=15, message="No cumple la longitud para el campo")
+    ])
+    nombre=StringField('Nombre',[
+        validators.DataRequired(message="El campo es requerido")
+    ])
+    apaterno=StringField('Apaterno',[mi_validacion])
     email=EmailField('Correo')
+
+
+class LoginForm(Form):
+    username=StringField('usuario',[
+        validators.DataRequired(message="El campo es requerido"),
+        validators.length(min=4,max=15, message="No cumple la longitud para el campo")
+    ])
+    password=StringField('password',[
+        validators.DataRequired(message="El campo es requerido"),
+        validators.length(min=4,max=15, message="No cumple la longitud para el campo")
+    ])
+
+class traductor(Form):
+    palabra=StringField('español',[
+        validators.DataRequired(message="El campo es requerido"),
+        validators.length(min=2,max=15, message="No cumple la longitud para el campo")
+    ])
+    word=StringField('ingles',[
+        validators.DataRequired(message="El campo es requerido"),
+        validators.length(min=2,max=15, message="No cumple la longitud para el campo")
+    ])
